712ed48be07ed1088268ac4abb6e3887bc6be41e
Anouxay Sichanh <ainthaly@gmail.com>
2022-11-18 11:43:53 -0500

    Revamped website

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ function PageTrans() {
     //btn click active class
     for (let i = 0; i < sectBtn.length; i++) {
         sectBtn[i].addEventListener('click', function () {
-            let currentBtn = document.querySelectorAll('.active-btn');
+            let currentBtn = document.querySelectorAll('active-btn');
             currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
+            this.className += ' .active-btn';
         })
     }
 
